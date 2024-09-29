298f9a121b406d4c17ee56a65ed8b0c41371a33c
Askar Karasawa <askar.karasaev@gmail.com>
2024-09-29 12:09:31 +0900

    adding mailgun

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+import {env} from "@strapi/utils";
+
+export default () => ({
+    email: {
+        config: {
+            provider: 'mailgun',
+            providerOptions: {
+                key: env('MAILGUN_API_KEY'),
+                domain: env('MAILGUN_DOMAIN'),
+            },
+            settings: {
+                defaultFrom: env('EMAIL_DEFAULT_FROM', 'defaultemail@example.com'),
+                defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'defaultemail@example.com'),
+            },
+        },
+    },
+});
